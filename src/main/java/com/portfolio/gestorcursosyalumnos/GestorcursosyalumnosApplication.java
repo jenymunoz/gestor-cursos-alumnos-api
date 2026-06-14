d62e11cb740e44e652b04585b2264e8d5275a149
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-06-14 17:56:36 +0200

    Primer commit que incluye la configuración del archivo .properties y un ejemplo del archivo .env

---
+++ src/main/java/com/portfolio/gestorcursosyalumnos/GestorcursosyalumnosApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gestorcursosyalumnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorcursosyalumnosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorcursosyalumnosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/gestorcursosyalumnos/GestorcursosyalumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gestorcursosyalumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorcursosyalumnosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
