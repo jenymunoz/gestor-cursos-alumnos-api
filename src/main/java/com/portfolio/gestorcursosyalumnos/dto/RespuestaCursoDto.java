52cd8587f55f512c185af3b2a6d97ebcad4cf3f6
Jeny Melissa Muñoz Mejia <jeny.munoz@educa.madrid.org>
2026-06-26 11:46:08 +0200

    Añadiendo ejemplos de salida en DTO de respuesta para los cursos

--- src/main/java/com/portfolio/gestorcursosyalumnos/dto/RespuestaCursoDto.java
+++ src/main/java/com/portfolio/gestorcursosyalumnos/dto/RespuestaCursoDto.java
@@ -9,10 +9,22 @@ import lombok.Getter;
 @Schema(description = "Clase encargada de mostrar los datos de un curso.")
 public class RespuestaCursoDto {
     @Schema(
-            description = "Id generado automáticamente"
+            description = "Id generado automáticamente",
+            example = "1"
     )
     private Long id;
+
+    @Schema(
+            example = "Introducción a la Programación"
+    )
     private String nombre;
+
+    @Schema(
+            example = "Estudiaremos los fundamentos de la programación."
+    )
     private String descripcion;
+    @Schema(
+            example = "120"
+    )
     private int duracion;
 }
